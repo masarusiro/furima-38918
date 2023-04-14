76851f6042f0bdf1e9dbb5bca0f3c92b8b2fbed7
masarusiro <huaz4906@gmail.com>
2023-04-14 22:47:49 +0900

    テストコードとバリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヴー]+\z/u }
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヴー]+\z/u }
   validates :birth_date, presence: true
+  validates :nickname, presence: true
 end
