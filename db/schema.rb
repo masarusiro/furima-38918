c882561be84114e9aebb1b895d9b26ad5f84b6b0
masarusiro <huaz4906@gmail.com>
2023-04-14 18:01:58 +0900

    ユーザー登録、ログイン機能

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2023_04_14_065051) do
     t.string "first_name", null: false
     t.string "last_name_kana", null: false
     t.string "first_name_kana", null: false
-    t.date "birth_day", null: false
+    t.date "birth_date", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
