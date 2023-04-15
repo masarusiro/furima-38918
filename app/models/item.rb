3d49fda81521569d8a5c55b0cb4dda87e67ec250
masarusiro <huaz4906@gmail.com>
2023-04-16 00:41:39 +0900

    ActiveHashで管理するモデルの作成とアソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,13 @@ class Item < ApplicationRecord
 
   
   belongs_to :user
+  has_one_sttached :image
+
+  #ActiveHash
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :prefecture
+  belongs_to :condition
+  belongs_to :shipping_cost
+  belongs_to :shipping_day
 end
