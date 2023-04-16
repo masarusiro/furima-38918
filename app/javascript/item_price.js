24a600ae5e3311d77264eeffa76646ded50adb13
masarusiro <huaz4906@gmail.com>
2023-04-16 20:49:08 +0900

    販売手数料と販売利益を表示させる(js)

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -7,9 +7,9 @@ window.addEventListener('load', () => {
   const inputValue = priceInput.value;
   const addTaxDom = document.getElementById("add-tax-price");
   const ProfitDom = document.getElementById("profit");
-  // 販売手数料を計算する処理
+  // 販売手数料を計算する処理を表示させるid=add-tax-priceに
   addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
-  // 出品価格から販売手数料を引く処理
+  // 出品価格から販売手数料を引く処理をid=profitに
   ProfitDom.innerHTML = inputValue - addTaxDom.innerHTML;
   })
 });
