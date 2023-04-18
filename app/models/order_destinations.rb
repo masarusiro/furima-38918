55b9fe6d31c2f733822c4e03ac17b62dcb5b849f
masarusiro <huaz4906@gmail.com>
2023-04-19 03:41:00 +0900

    バリデーション

--- app/models/order_destinations.rb
+++ app/models/order_destinations.rb
@@ -2,9 +2,13 @@ class OrderDestinations
   include ActiveModel::Model
 
    with_options presense: true do
+    #orderモデル
     validates :user_id
     validates :item_id
-
-    
-  
+    #destinationモデル
+    validates :postcode
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :city
+    validates :block
+    validates :phone_number 
   end
