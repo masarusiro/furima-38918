3589b7193f7a654d1255d77cae1a4f136bdce539
masarusiro <huaz4906@gmail.com>
2023-04-19 21:08:09 +0900

    PAYJPの導入

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,6 +8,7 @@ class OrdersController < ApplicationController
       @item = Item.find(params[:item_id])
       @order_destinations = OrderDestinations.new(order_params)
       if @order_destinations.valid?
+         pay_item
          @order_destinations.save
          redirect_to root_path
       else
@@ -18,7 +19,17 @@ class OrdersController < ApplicationController
 private
 
 def order_params
-  params.require(:order_destinations).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  params.require(:order_destinations).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id]), token: params[:token]
 end
+
+def pay_item
+  Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # PAY.JPテスト秘密鍵
+  Payjp::Charge.create(
+    amount: order_params[:price],  # 商品の値段
+    card: order_params[:token],    # カードトークン
+    currency: 'jpy'                 # 通貨の種類（日本円）
+  )
+end  
+
 
 end
